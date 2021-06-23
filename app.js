b890eff587331a79fb4b1a239239e88c93faaf9c
Jose-Antonio Rubio <jose-antonio.rubio@iii.com>
2021-06-23 15:37:06 -0400

    connected to mongodb and refactored get to pull all books

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-
+const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('success!')
+const db = mongoose.connect("mongodb://localhost/bookAPI");
+const Book = require("./models/bookModel");
+
+bookRouter.route("/books").get((req, res) => {
+  Book.find((err, books) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json(books);
+    }
+  });
+});
+
+app.use("/api", bookRouter);
+
+app.get("/", (req, res) => {
+  res.send("success!");
 });
 
 app.listen(port, () => {
-    console.log('Listening on port ', port);
+  console.log("Listening on port ", port);
 });
