c16f40e2b35b12fcb06a8811370b48680b770492
Jose-Antonio Rubio <jose-antonio.rubio@iii.com>
2021-06-24 10:51:18 -0400

    refactored code to have routes in separate file

--- app.js
+++ app.js
@@ -3,46 +3,14 @@ const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 
 const app = express();
-const bookRouter = express.Router();
+const Book = require("./models/bookModel");
+const bookRouter = require('./routes/bookRouter')(Book);
 const port = process.env.PORT || 3000;
 
 const db = mongoose.connect("mongodb://localhost/bookAPI");
-const Book = require("./models/bookModel");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-bookRouter.route("/books")
-  .get((req, res) => {
-    const query = {};
-    if(req.query.genre){
-      query.genre = req.query.genre;
-    }
-
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      } else {
-        return res.json(books);
-      }
-    });
-})
-.post((req, res) => {
-  const book = new Book(req.body);
-
-  book.save();
-  return res.status(201).json(book);
-});
-
-bookRouter.route("/books/:bookId").get((req, res) => {
-  Book.findById(req.params.bookId, (err, book) => {
-    if (err) {
-      return res.send(err);
-    } else {
-      return res.json(book);
-    }
-  });
-});
 
 app.use("/api", bookRouter);
 
